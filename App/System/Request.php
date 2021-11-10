368ea46961617ca532323855e6af9f0d1ba95a83
tucno21 <carlitostucno@gmail.com>
2021-11-10 17:17:24 -0500

    cambios Request

--- App/System/Request.php
+++ App/System/Request.php
@@ -22,26 +22,26 @@ class Request
         return strtolower($_SERVER['REQUEST_METHOD']);
     }
 
+
     public function isGet()
     {
-        $data = [];
-        if ($this->methodWeb() === 'get') {
-            foreach ($_GET as $key => $value) {
-                $data[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
-            }
-        }
-
-        if (!empty($_FILES)) {
-            $data = array_merge($data, $_FILES);
-        }
-
-        return $data;
+        return $this->getMethod() === 'get';
     }
 
     public function isPost()
     {
+        return $this->getMethod() === 'post';
+    }
+
+    public function getInput()
+    {
         $data = [];
-        if ($this->methodWeb() === 'post') {
+        if ($this->isGet()) {
+            foreach ($_GET as $key => $value) {
+                $data[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+        }
+        if ($this->isPost()) {
             foreach ($_POST as $key => $value) {
                 $data[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
             }
